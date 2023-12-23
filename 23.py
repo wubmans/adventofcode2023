b74841ee3a9dc3c4d904a2a6f474dcb7f1f30eda
wubmans <wubmans@outlook.com>
2023-12-23 16:15:14 +0100

    poekie day 23 p 2

--- 23.py
+++ 23.py
@@ -8,9 +8,15 @@ def print_grid(grid):
         
 
 def search_grid(grid):
+
+    touched_grid = grid.copy()
+
+    vertices = [(1,0), (21, 22)]
+    edges = {}
     
     paths = [[(1, 0)]]
 
+    forks = []
     done_paths = []
 
     new_point = True
@@ -23,77 +29,105 @@ def search_grid(grid):
 
         for path in paths:
 
-            p = path[-1]
+            while True:
 
-            pps = []
+                p = path[-1]
 
-            for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                point = (p[0] + dir[0], p[1] + dir[1])
 
-                if point[0] not in range(len(grid)) or point[1] not in range(len(grid[0])):
+                pps = []
+
+                for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    point = (p[0] + dir[0], p[1] + dir[1])
+
+                    if point[0] not in range(len(grid)) or point[1] not in range(len(grid[0])):
+                        continue
+
+                    c = grid[point[0]][point[1]]
+
+                    if c == '#':
+                        continue
+
+                    if point in path:
+                        continue
+
+                    # if touched_grid[point[0]][point[1]] == 'x':
+                    #     continue
+
+                    # touched_grid[point[0]][point[1]] = 'x'
+
+                    pps.append(point)
+
+                if len(pps) == 1:
+                    path.append(pps[0])
                     continue
 
-                c = grid[point[0]][point[1]]
+                if (path[0], p) in edges or (p, path[0]) in edges:
+                    break
 
-                if c == '#':
-                    continue
+                edges[(path[0], p)] = len(path) - 1
 
-                # if c == '>' and dir == (-1, 0):
-                #     continue
+                if len(pps) == 0:                
+                    break
+                # we hit a fork
 
-                # if c == 'v' and dir == (0, -1):
-                #     continue
+                if p not in vertices:
+                    vertices.append(p)
 
-                if point in path:
-                    continue
-
-                pps.append(point)
-
-                # new_point = True
-            
-                # new_path = path.copy()
-                # new_path.append(point)
-
-                # new_paths.append(new_path)
-
-            if len(pps) > 1:
-                done_paths.append(path)
-                new_paths = [ [point, p] for p in pps]
+    
+                new_paths += [[p, ps] for ps in pps]
                 new_point = True
-                
-            elif len(pps) == 1:
-                np = path.copy()
-                np.append(pps[0])
-                new_paths.append(np)
-                new_point = True
+                break
 
         paths = new_paths
 
+    # for edge in list(edges):
+    #     if edge[0] not in vertices or edge[-1] not in vertices:
+    #         del edges[edge]
+
+
 
     max_length = -1
-    lp = None
+   
+    paths = [[[(1,0)], 0]]
+
+    changed = True
+    
+    while changed:
+        changed = False
+
+        new_paths = []
+
+        for path in paths:
+
+            pathChanged = False
+
+            for edge in edges:
+                if path[0][-1] in edge:
+                    p = edge[0] if path[0][-1] == edge[1] else edge[1]
+                    if p not in path[0]:
+                        new_path = [path[0].copy(), path[1]]
+                        new_path[0].append(p)
+                        new_path[1] += edges[edge]
+                        new_paths.append(new_path)
+                        changed = True
+                        pathChanged = True
+
+            if pathChanged == False:
+                new_paths.append(path)           
+                        
+        if changed:
+            paths = new_paths
+
+    print()
+    print()
 
     for path in paths:
-        pp = path[-1]
-        if pp[1] != len(grid[0]) -1:
-            continue
-
-        if len(path) > max_length:
-            lp = path
-        max_length = max(max_length, len(path) - 1)
-
-    print()
-    print()
-
-    if lp == None:
-        exit()
-
-    for p in lp:
-        grid[p[0]][p[1]] = 'o'
-
+        if path[1] > max_length or max_length == -1:
+            max_length = path[1]
+  
     print("max length: %s" % (max_length))
 
-    print_grid(grid)
+    # print_grid(grid)
 
 for y, line in enumerate(open('23input.txt').readlines()):
     line = line.strip()
